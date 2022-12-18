b825b5429e9935e6d37a955efbe63dceef3accbc
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-17 17:46:23 -0700

    Added basic path to find all users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// path to create user
+// path to get all user info (for dev use only)
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll();
+        
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// path to create user (sign up)
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -12,15 +23,10 @@ router.post('/', async (req, res) => {
     }
 });
 
-// path to get all user info
-router.get('/', async (req, res) => {
-    try {
+// TODO: login path
 
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
+// TODO: profile path? (if time allows)
 
-
+// TODO: logout path
 
 module.exports = router;
