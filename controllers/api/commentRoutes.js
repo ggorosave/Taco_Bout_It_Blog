432f2651277ed2b892a8e7f2b6657e12e2dfef23
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-17 22:00:21 -0700

    Added routes to update or delete a comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -43,7 +43,18 @@ router.post('/', async (req, res) => {
 // route to update a comment (if time allows)
 router.put('/:id', async (req, res) => {
     try {
+        const updatedComment = await Comment.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
 
+        if (!updatedComment[0]) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+
+        res.status(200).json(updatedComment);
     } catch (err) {
         res.status(400).json(err);
     }
@@ -52,7 +63,18 @@ router.put('/:id', async (req, res) => {
 // route to delete a comment (if time allows)
 router.delete('/:id', async (req, res) => {
     try {
+        const deletedComment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
 
+        if (!deletedComment) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+
+        res.status(200).json(deletedComment);
     } catch (err) {
         res.status(400).json(err);
     }
